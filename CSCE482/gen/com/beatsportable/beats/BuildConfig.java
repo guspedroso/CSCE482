bc1c6ba996d44b410d489c771f7bf5a949049206
Elizabeth <echlipala@tamu.edu>
2015-04-15 16:16:19 -0500

    Updated Menu UI and Added Songs to Database
    - Goal now shows up in the menu
    - BPM number is now the same font as everything else
    - Added many songs into song DB

--- CSCE482/gen/com/beatsportable/beats/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.beatsportable.beats;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- CSCE482/scripts/images/rotateimgs.py
+++
@@ -1,8 +0,0 @@
-import Image, glob
-files = glob.glob("*.png")
-for filename in files:
-	im = Image.open(filename)
-	rotations = {"up": 0, "left":90, "down":180, "right":270}
-	for name, deg in rotations.iteritems():
-		im.rotate(deg).save("out/arrow_%s_%s" % (name,filename), "PNG")
-
